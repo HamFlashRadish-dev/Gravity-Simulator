43020033b60eb2b21e24f6be593221cb977e96e9
HamFlashRadish-dev <griffinhansberg@gmail.com>
2025-11-30 18:48:53 -0500

    Not much new progress but made new variables for mouse dow, if mouse in window, if touching ball, if ball is touching ground

--- main.cpp
+++ main.cpp
@@ -10,6 +10,10 @@ int main() {
     float fallVelocity = 0.0f;
     float mouseX;
     float mouseY;
+    bool touchingBall = false;
+    bool mouseDown = false;
+    bool mouseInWindow = false;
+    bool ballHittingGround = false;
 
     sf::RenderWindow window(sf::VideoMode(800, 600), "Test");
 
@@ -34,15 +38,61 @@ int main() {
         mouseX = static_cast<float>(mousePos.x);
         mouseY = static_cast<float>(mousePos.y);
 
-        
+        //Checks if mouse down
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left)){
+            mouseDown = true;
+        }
+        else {
+            mouseDown = false;
+        }
+        // Check if mouse is within window bounds
+        if (mouseX >= 0 && mouseX <= 800 - radius * 2 && mouseY >= 0 && mouseY <= 600) {
+            mouseInWindow = true;
+        }
+        else {
+            mouseInWindow = false;
+        }
 
-        if (ballY <= 600 - radius * 2) {
-            fallVelocity += 10.f;
+        //Checks if mouse is touching ball
+        if (mouseX >= ballX && mouseX <= ballX + radius * 2 && mouseY >= ballY && mouseY <= ballY + radius * 2) {
+            touchingBall = true;
+        }
+        else {
+            touchingBall = false;
+        }
+
+        //Checks if ball is hitting ground
+        if (ballY >= 600 - radius * 2) {
+            ballHittingGround = true;
+        }
+        else {
+            ballHittingGround = false;
+        }
+        
+        // Reset ball position on R key press
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::R)) {
+            ballX = 400 - radius;
+            ballY = 200 - radius;
+            fallVelocity = 0.0f;
+            ball.setPosition(ballX, ballY);
+        }
+
+        if (mouseDown && touchingBall && mouseInWindow) {
+            ballX = mouseX - radius;
+            ballY = mouseY - radius;
+            fallVelocity = 0.0f;
+            ball.setPosition(ballX, ballY);
+        }
+        
+        if (!ballHittingGround) {
+            fallVelocity += 7.5f;
             ballY += fallVelocity * dt;
             ball.setPosition(ballX, ballY);
         }
         else {
             fallVelocity = 0.0f;
+            ballY = 600 - radius * 2;
+            ball.setPosition(ballX, ballY);
         }
 
         window.clear();
