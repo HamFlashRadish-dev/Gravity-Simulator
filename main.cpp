cefceeededad90de757cceed6dc896e129f65d83
HamFlashRadish-dev <griffinhansberg@gmail.com>
2025-11-30 13:47:48 -0500

    First code change text and added circle

--- main.cpp
+++ main.cpp
@@ -1,14 +1,28 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 
 int main() {
-    sf::RenderWindow win(sf::VideoMode(800, 600), "Test");
-    while (win.isOpen()) {
+    int radius = 50;
+
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Test");
+
+    sf::CircleShape ball(radius);
+    ball.setFillColor(sf::Color::White);
+    ball.setPosition(400 - radius / 2, 300 - radius / 2);
+
+    while (window.isOpen()) {
         sf::Event e;
-        while (win.pollEvent(e)) {
+        while (window.pollEvent(e)) {
             if (e.type == sf::Event::Closed)
-                win.close();
+                window.close();
         }
-        win.clear();
-        win.display();
+
+        window.clear();
+
+        window.draw(ball);
+
+        window.display();
     }
+
+    return 0;
 }
