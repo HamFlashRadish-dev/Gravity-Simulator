7de0cc65711b87d1295fe7036aa9cf43d110fd7b
HamFlashRadish-dev <griffinhansberg@gmail.com>
2025-11-30 13:28:01 -0500

    Initial commit for gravity simulator

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+    sf::RenderWindow win(sf::VideoMode(800, 600), "Test");
+    while (win.isOpen()) {
+        sf::Event e;
+        while (win.pollEvent(e)) {
+            if (e.type == sf::Event::Closed)
+                win.close();
+        }
+        win.clear();
+        win.display();
+    }
+}
