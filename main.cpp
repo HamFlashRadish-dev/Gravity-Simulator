d0c33848c4458247396ea3c9b99631f99e44cc7c
HamFlashRadish-dev <griffinhansberg@gmail.com>
2025-11-30 15:54:22 -0500

    Basic gravity simulation added

--- main.cpp
+++ main.cpp
@@ -1,14 +1,25 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
+#include <ctime>
+#include <cstdlib>
 
 int main() {
-    int radius = 50;
+    int radius = 15;
+    float ballX = 400 - radius;
+    float ballY = 200 - radius;
+    float fallVelocity = 0.0f;
+    float mouseX;
+    float mouseY;
 
     sf::RenderWindow window(sf::VideoMode(800, 600), "Test");
 
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+
     sf::CircleShape ball(radius);
     ball.setFillColor(sf::Color::White);
-    ball.setPosition(400 - radius / 2, 300 - radius / 2);
+    ball.setPosition(ballX, ballY);
+
+    sf::Clock clock;
 
     while (window.isOpen()) {
         sf::Event e;
@@ -17,8 +28,26 @@ int main() {
                 window.close();
         }
 
+        double dt = clock.restart().asSeconds();
+
+        sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+        mouseX = static_cast<float>(mousePos.x);
+        mouseY = static_cast<float>(mousePos.y);
+
+        
+
+        if (ballY <= 600 - radius * 2) {
+            fallVelocity += 10.f;
+            ballY += fallVelocity * dt;
+            ball.setPosition(ballX, ballY);
+        }
+        else {
+            fallVelocity = 0.0f;
+        }
+
         window.clear();
 
+        ball.setPosition(ballX, ballY);
         window.draw(ball);
 
         window.display();
